4a3afe32f141fb57f2e8ec49c6d222d290fef64d
Hir0S4ns <140856305+Hir0S4ns@users.noreply.github.com>
2023-08-08 00:54:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
-// Array com as respostas corretas de cada fase
-const answers = ["xandão", "fake", "natty", "fim"];
+const answers = ["pirâmide","video-game","antigo", "quinta-feira", "ferro", "musica", "agosto", "3 patos"];
 
-// Array com os caminhos das imagens para cada fase
-const imagePaths = ["images/xandão.jpg", "images/faker.jpeg", "images/naatty.jpg"];
+var phaseImages = [
+    "imagens/piramide.png",
+    "imagens/video game.png",
+    "imagens/nokia.png",
+    "imagens/quinta-feira.png",
+    "imagens/tiberio claudio cesar - ferro.png",
+    "imagens/musica.png",
+    "imagens/agosto.png",
+    "imagens/3patos.png"
 
-// Variável para controlar a fase atual
-let currentPhase = 0;
 
-// Função para atualizar a imagem da fase
-function updatePhaseImage() {
-    const phaseImage = document.getElementById("phaseImage");
-    phaseImage.src = imagePaths[currentPhase];
-    phaseImage.alt = "Fase " + (currentPhase + 1);
+];
+
+var currentPhase = 0;
+
+function changePhaseImage() {
+    if (currentPhase < phaseImages.length) {
+        var imageElement = document.getElementById("phaseImage");
+        imageElement.src = phaseImages[currentPhase];
+    }
 }
 
-// Função para verificar a resposta
 function checkAnswer() {
     const userAnswer = document.getElementById("answer").value.toLowerCase().trim();
 
     if (userAnswer === answers[currentPhase]) {
-        // Resposta correta, vamos avançar para a próxima fase
         currentPhase++;
 
         if (currentPhase === answers.length) {
-            // Fim do enigma
             document.getElementById("feedback").innerText = "Parabéns, você completou o enigma!";
             document.getElementById("answer").disabled = true;
         } else {
-            // Próxima fase
             document.getElementById("feedback").innerText = "Resposta correta! Avance para a próxima fase.";
             document.getElementById("answer").value = "";
-            updatePhaseImage();
+            changePhaseImage();
         }
     } else {
-        // Resposta incorreta
         document.getElementById("feedback").innerText = "Resposta incorreta, tente novamente.";
     }
 }
 
-// Atualiza a imagem da fase inicial ao carregar a página
-updatePhaseImage();
+// Inicializar a imagem da primeira fase
+changePhaseImage();
+// ... (código anterior)
+
+function changePhaseImage() {
+    if (currentPhase < phaseImages.length) {
+        var imageElement = document.getElementById("phaseImage");
+        imageElement.src = phaseImages[currentPhase];
+
+        // Mostrar ou esconder o texto introdutório
+        var introText = document.getElementById("introText");
+        if (currentPhase === 0) {
+            introText.style.display = "block"; // Mostrar na primeira fase
+        } else {
+            introText.style.display = "none"; // Esconder nas próximas fases
+        }
+    }
+}
+
+// ... (código posterior)
